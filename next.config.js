c2d2c492f5fceb30830dadd43b452c1036bf36f2
Sofía de Monasterio <14180854-sofiademonasterio1@users.noreply.gitlab.com>
2023-05-06 20:15:41 -0300

    updated package.json

--- next.config.js
+++ next.config.js
@@ -5,4 +5,9 @@ const nextConfig = {
     }
 }
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/next-gitlab-pages/' : '',
+  trailingSlash: true
+}
