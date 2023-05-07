a27509a1a2887336808adc8376b56c710721837e
Sofía de Monasterio <14180854-sofiademonasterio1@users.noreply.gitlab.com>
2023-05-06 21:37:21 -0300

    fix config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     images  : {
         domains: ['api.dicebear.com']
-    },
-    output: 'export',
-    assetPrefix: "https://sofiademonasterio1.gitlab.io/next.js-practice/"
+    }
 }
 
 module.exports = nextConfig;
